671423d27cce69b4a407c7bbd461296f378315fa
Adam Nguyễn <contact.adamnguyen@gmail.com>
2024-12-28 15:52:31 +0700

    update SQL 3:52 PM 28.12.2024

--- GroupProject/Quan_ly_san_the_thao/IT8_inster_data_query.sql
+++ GroupProject/Quan_ly_san_the_thao/IT8_inster_data_query.sql
@@ -3,7 +3,34 @@
 INSERT INTO SANTHETHAO VALUES (
     'SANTT01',
     'BONGDA',
-    'San bong da Thanh Manchester',
+    'San bong da 1',
+    170,
+    150,
+    200
+)
+
+INSERT INTO SANTHETHAO VALUES (
+    'SANTT01',
+    'BONGCHUYEN',
+    'San bong chuyen 1',
+    170,
+    150,
+    200
+)
+
+INSERT INTO SANTHETHAO VALUES (
+    'SANTT01',
+    'BONGRO',
+    'San bong ro 1',
+    170,
+    150,
+    200
+)
+
+INSERT INTO SANTHETHAO VALUES (
+    'SANTT01',
+    'CAULONG',
+    'San cau long 1',
     170,
     150,
     200
@@ -11,8 +38,35 @@ INSERT INTO SANTHETHAO VALUES (
 
 INSERT INTO SANTHETHAO VALUES (
     'SANTT02',
+    'BONGDA',
+    'San bong da 2',
+    200,
+    160,
+    230
+)
+
+INSERT INTO SANTHETHAO VALUES (
+    'SANTT02',
     'BONGCHUYEN',
-    'San bong chuyen Nagoya',
+    'San bong chuyen 2',
+    200,
+    160,
+    230
+)
+
+INSERT INTO SANTHETHAO VALUES (
+    'SANTT02',
+    'BONGRO',
+    'San bong ro 2',
+    200,
+    160,
+    230
+)
+
+INSERT INTO SANTHETHAO VALUES (
+    'SANTT02',
+    'CAULONG',
+    'San cau long 2',
     200,
     160,
     230
@@ -20,26 +74,44 @@ INSERT INTO SANTHETHAO VALUES (
 
 INSERT INTO SANTHETHAO VALUES (
     'SANTT03',
-    'BONGRO',
-    'San bong ro New York',
+    'BONGDA',
+    'San bong da 3',
     150,
     130,
     180
 )
 
 INSERT INTO SANTHETHAO VALUES (
-    'SANTT04',
+    'SANTT03',
+    'BONGCHUYEN',
+    'San bong chuyen 3',
+    150,
+    130,
+    180
+)
+
+INSERT INTO SANTHETHAO VALUES (
+    'SANTT03',
+    'BONGRO',
+    'San bong ro 3',
+    150,
+    130,
+    180
+)
+
+INSERT INTO SANTHETHAO VALUES (
+    'SANTT03',
     'CAULONG',
-    'San cau long Thuong Hai',
-    100,
-    50,
-    130
+    'San cau long 3',
+    150,
+    130,
+    180
 )
 
 INSERT INTO KHACHHANG VALUES 
 (
 	'admin',
-	'gloryglorymanunited',
+	'glorygloryMU',
 	N'Nguyễn Võ Ngọc Bảo',
 	'contact.adamnguyen@gmail.com',
 	'0329501759',
--- GroupProject/Quan_ly_san_the_thao/IT8_trigger_query.sql
+++ GroupProject/Quan_ly_san_the_thao/IT8_trigger_query.sql
@@ -1,7 +1,7 @@
--- Đảm bảo các thông nhập vào của một khách hàng không được phép NULL và nằm trong giới hạn ký tự
-    CREATE TRIGGER TRIGGER_DAMBAO_THONGTIN_DUNG_DU
+-- Các thông tin của một khách hàng không được phép trống, đúng định dạng (ví dụ email phải có @gmail.com, sdt luôn bắt đầu bằng số 0) và nằm trong giới hạn ký tự.
+    CREATE TRIGGER TRIGGER_THONGTIN_KHACHHANG
     ON KHACHHANG
-    AFTER 
+    BEFORE 
         INSERT,
         UPDATE
     AS 
@@ -33,7 +33,7 @@
             @GTINH = NULL
         )
             BEGIN
-                RAISERROR('Thong tin nhap vao khong duoc phep bo trong', 16, 1)
+                RAISERROR(N'Thông tin khách hàng không được phép bỏ trống!', 16, 1)
                 ROLLBACK TRANSACTION
             END
         ELSE IF (
@@ -46,113 +46,36 @@
             LEN(@SDT) > 10
         )
             BEGIN
-                RAISERROR('Thong tin nhap vao vuot qua ky tu cho phep', 16, 1)
+                RAISERROR(N'Thông tin nhập quá ký tự cho phép!', 16, 1)
                 ROLLBACK TRANSACTION
             END
         ELSE IF (
-            @GTINH NOT IN (1, 0)
-        )
-            BEGIN
-                RAISERROR('Gioi tinh chi co the la 1 (Nam) va 0 (Nu)', 16, 1)
-                ROLLBACK TRANSACTION
-            END
-        ELSE IF (
-            LEN(@PASSWRD) < 10
+            LEN(@PASSWRD) < 7
             OR
             LEN(@TENKH) < 2
             OR
-            LEN(@EMAIL) < 2
+            LEN(@EMAIL) < 10
             OR
             LEN(@SDT) < 8
         )
             BEGIN
-                RAISERROR('Thong tin nhap vao khong du do dai toi thieu cho phep', 16, 1)
+                RAISERROR(N'Thông tin chưa đủ độ dài tối thiểu cho phép!', 16, 1)
                 ROLLBACK TRANSACTION
             END
+		ELSE IF (
+			@EMAIL NOT LIKE '%@gmail.com'
+		)
+			BEGIN
+				RAISERROR(N'Email chưa chính xác!', 16, 1)
+				ROLLBACK TRANSACTION
+			END
+		ELSE IF (
+			LEFT(@SDT, 1) <> '0'
+		)
+			BEGIN
+				RAISERROR(N'Số điện thoại chưa chính xác!', 16, 1)
+				ROLLBACK TRANSACTION
+			END
     END
     GO
--- Đảm bảo rằng mỗi sân thể thao chỉ sử dụng cho một môn thể thao duy nhất
-    CREATE TRIGGER TRIGGER_DAMBAO_1SANTHETHAO_1MONTHETHAO
-    ON SANTHETHAO
-    AFTER 
-        INSERT,
-        UPDATE
-    AS 
-    BEGIN 
-        DECLARE 
-            @DEMTRUNGLAP INT
-        
-        SELECT @DEMTRUNGLAP = COUNT(*)
-        FROM (
-            SELECT SANTHETHAO.MASANTT
-            FROM SANTHETHAO
-            GROUP BY SANTHETHAO.MASANTT
-            HAVING COUNT(DISTINCT SANTHETHAO.MONTHETHAO) > 1
-        ) AS DEMTRUNGLAPTABLE;
 
-        IF (
-            @DEMTRUNGLAP > 0
-        )
-            BEGIN
-                RAISERROR('Moi san the thao chi duoc gan lien voi 1 mon the thao duy nhat', 16, 1)
-                ROLLBACK TRANSACTION
-            END
-    END
-    GO
--- Đảm bảo rằng giá thuê sân vào buổi trưa là nhỏ nhất trong số 3 giá thuê sân, giá thuê sân sáng và tối chỉ có thể lệch nhau 30k
-    CREATE TRIGGER TRIGGER_DAMBAO_CHENHLECH_GIATHUE
-    ON SANTHETHAO
-    AFTER 
-        INSERT,
-        UPDATE
-    AS 
-    BEGIN 
-        IF EXISTS (
-            SELECT *
-            FROM INSERTED
-            WHERE (
-                INSERTED.GTTRUA >= INSERTED.GTSANG
-                OR
-                INSERTED.GTTRUA >= INSERTED.GTTOI
-            )
-        )
-            BEGIN
-                RAISERROR('Gia thue san vao buoi trua phai la gia thue nho nhat trong so 3 gia thue sang, trua, toi', 16, 1)
-                ROLLBACK TRANSACTION
-            END
-        ELSE IF EXISTS (
-            SELECT *
-            FROM INSERTED
-            WHERE ABS(INSERTED.GTSANG - INSERTED.GTTOI) > 30000
-        )
-            BEGIN
-                RAISERROR('Gia thue san vao buoi sang va buoi toi chi duoc phep chenh lech nhau 30k', 16, 1)
-                ROLLBACK TRANSACTION
-            END
-    END
-    GO
--- Đảm bảo rằng ngày hóa đơn có hiệu lực chỉ nằm trong khoảng ngày thanh toán đến ngày thanh toán + 7
-    CREATE TRIGGER TRIGGER_DAMBAO_HIEULUC_HOADON
-    ON CTHD
-    AFTER 
-        INSERT,
-        UPDATE
-    AS 
-    BEGIN 
-        IF EXISTS (
-            SELECT *
-            FROM INSERTED
-            JOIN HOADON
-                ON INSERTED.MAHD = HOADON.MAHD
-            WHERE (
-                INSERTED.NGHDHLUC < HOADON.NGTTOAN
-                OR
-                INSERTED.NGHDHLUC > DATEADD(DAY, 7, HOADON.NGTTOAN)
-            )
-        )
-            BEGIN
-                RAISERROR('Hoa don chi co hieu luc trong vong 7 ngay tinh tu ngay thanh toan', 16, 1)
-                ROLLBACK TRANSACTION
-            END
-    END
-    GO
